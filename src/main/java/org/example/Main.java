a7f7d6ba82e4ef11851e03e5c50e2b9db817b401
kirillnamestnikov <kir.namestnikov@gmail.com>
2025-03-19 19:07:45 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.example.TestName;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TestName.class);
